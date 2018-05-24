ffffc18a141911b1fda15b9c40000f46c0cb56b9
TIEYU <15301074@bjtu.edu.cn>
2018-05-25 01:18:07 +0800

    孟鑫磊——小游戏3——1.0
     基本可以运行
    具体细节需要调整的一个版本
    需要叶了望进行触发器的设施

---
+++ Homing/Assets/shut_down.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shut_down : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator CountSeconds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            PlayerController.reduceAttr(100, PlayerController.attrType.mSpeed);
+            Debug.Log("why not");
+            StopCoroutine("CountSeconds");
+        }
+    }
+
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.CompareTag("Player"))
+        {
+
+            InvokeRepeating("die7", 0, 1f);
+            PlayerController.reduceAttr(5, PlayerController.attrType.mSpeed);
+
+            StartCoroutine("CountSeconds");
+            Debug.Log("why not222");
+
+
+        }
+    }
+    void OnTriggerExit(Collider Col)
+    {
+        if (Col.CompareTag("Player"))
+        {
+            Debug.Log("heya444");
+            CancelInvoke("die7");
+
+            PlayerController.addAttr(5, PlayerController.attrType.mSpeed);
+
+        }
+    }
+
+    private void die7()
+    {
+        PlayerController.reduceAttr(5, PlayerController.attrType.HP);
+    }
+}
